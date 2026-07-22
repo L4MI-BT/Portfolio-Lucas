b5cb045d7591ff16d805bb49f7e7c391656fa0c9
Lucas Blanchard-Toulet <l.blanchard-toulet@outlook.fr>
2026-07-22 17:10:38 +0200

    Section Contact avec icônes Font Awesome et mise en page en 3 blocs

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,28 +1,44 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope, faPhone, faFileArrowDown } from '@fortawesome/free-solid-svg-icons'
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+
 function Contact() {
   return (
     <section id="contact">
-      <h2>Contact</h2>
+      <h2 className="text-center mb-5">Contact</h2>
 
-      <ul className="contact-liens">
-        <li>
-          <a href="mailto:l.blanchard-toulet@outlook.fr">
-            l.blanchard-toulet@outlook.fr
+      <div className="contact-content">
+
+        <a href="mailto:l.blanchard-toulet@outlook.fr" className="contact-email">
+          <FontAwesomeIcon icon={faEnvelope} className="me-2" />
+          l.blanchard-toulet@outlook.fr
+        </a>
+
+        <a href="/BLANCHARD-TOULET_LUCAS_CV_alternance.pdf" target="_blank" rel="noopener noreferrer" className="btn btn-primary contact-cv">
+          <FontAwesomeIcon icon={faFileArrowDown} className="me-2" />
+          Télécharger mon CV
+        </a>
+
+        <div className="contact-links">
+
+          <a href="tel:+33781226680" className="contact-link">
+            <FontAwesomeIcon icon={faPhone} />
+            07 81 22 66 80
           </a>
-        </li>
-        <li>
-          <a href="tel:+33781226680">07 81 22 66 80</a>
-        </li>
-        <li>
-          <a href="https://github.com/L4MI-BT" target="_blank" rel="noopener noreferrer">
-            GitHub
-          </a>
-        </li>
-        <li>
-          <a href="https://www.linkedin.com/in/lucas-blanchard-toulet-181044240/" target="_blank" rel="noopener noreferrer">
+
+          <a href="https://www.linkedin.com/in/lucas-blanchard-toulet-181044240/" target="_blank" rel="noopener noreferrer" className="contact-link">
+            <FontAwesomeIcon icon={faLinkedin} />
             LinkedIn
           </a>
-        </li>
-      </ul>
+
+          <a href="https://github.com/L4MI-BT" target="_blank" rel="noopener noreferrer" className="contact-link">
+            <FontAwesomeIcon icon={faGithub} />
+            GitHub
+          </a>
+
+        </div>
+
+      </div>
     </section>
   )
 }
